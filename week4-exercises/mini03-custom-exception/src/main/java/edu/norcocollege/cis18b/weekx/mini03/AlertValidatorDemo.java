80e87ff6cb00a3656f9b3d27261d7fc26c348c38
Ikhlas A. Taj Stanikzai <itajstanik1@student.rccd.edu>
2026-04-11 19:34:19 +0000

    Completed mini03 assignment

--- week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertValidatorDemo.java
+++ week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/AlertValidatorDemo.java
@@ -3,8 +3,14 @@ package edu.norcocollege.cis18b.weekx.mini03;
 public class AlertValidatorDemo {
 
     public static void validateAlert(Alert alert) throws InvalidAlertException {
-        // TODO: Throw InvalidAlertException if the message is null or blank.
-        // TODO: Throw InvalidAlertException if the level is null.
+
+        if (alert.getMessage() == null || alert.getMessage().trim().isEmpty()) {
+            throw new InvalidAlertException("Alert message cannot be empty.");
+        }
+
+        if (alert.getLevel() == null) {
+            throw new InvalidAlertException("Alert level cannot be null.");
+        }
     }
 
     public static void main(String[] args) {
