657a81dcb335f75b5a9bd0ddf249e5879640caaa
Ikhlas A. Taj Stanikzai <itajstanik1@student.rccd.edu>
2026-04-13 07:41:07 +0000

    Completed mini06 assignment

--- week4-exercises/mini06-exception-translation/src/main/java/edu/norcocollege/cis18b/weekx/mini06/InMemoryAlertRepository.java
+++ week4-exercises/mini06-exception-translation/src/main/java/edu/norcocollege/cis18b/weekx/mini06/InMemoryAlertRepository.java
@@ -1,14 +1,16 @@
 package edu.norcocollege.cis18b.weekx.mini06;
 
 public class InMemoryAlertRepository implements AlertRepository {
+
     @Override
     public void save(Alert alert) throws AlertStorageException {
         try {
-            // TODO: Simulate a low-level failure.
-            // Example: throw new RuntimeException("Database connection failed");
+            
+            throw new RuntimeException("Database connection failed");
+
         } catch (RuntimeException ex) {
-            // TODO: Translate the exception into AlertStorageException.
-            // Preserve the original cause.
+            
+            throw new AlertStorageException("Failed to save alert.", ex);
         }
     }
 }
